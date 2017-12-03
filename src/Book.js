3467418f0fd5ba80ba5a1ff97d422cb6f32037ec
wuyiqun0718 <wuyiqun0718@gmail.com>
2017-12-03 13:34:20 -0600

    fix the bug that can't let user clear the shelf of a book

--- src/Book.js
+++ src/Book.js
@@ -1,35 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Book extends React.Component {
-	static propTypes = {
-	    book: PropTypes.object.isRequired,
-	    onShelfChange: PropTypes.func.isRequired
-	}
-	moveBook = (shelf, e) => {
+const Book = props => {
+	const moveBook = (shelf, e) => {
 		const selectedShelf = e.target.value;
 		if (selectedShelf === shelf) return;
-		this.props.onShelfChange(selectedShelf);
+		props.onShelfChange(selectedShelf);
 	}
-	render() {
-		const { book } = this.props;
-		const { title, imageLinks = {}, authors = [], shelf = 'none' } = book;
-		return (
-			<div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${imageLinks.smallThumbnail}")` }}></div>
-                <div className="book-shelf-changer">
-                  <select value={shelf} onChange={this.moveBook.bind(null, shelf)}>
-                    <option value="moveTo" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">{title}</div>
-              <div className="book-authors">{authors[0]}</div>
-            </div>)
-	}
+	const { book } = props;
+	const { title, imageLinks = {}, authors = [], shelf = 'none' } = book;
+	return (
+		<div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${imageLinks.smallThumbnail}")` }}></div>
+            <div className="book-shelf-changer">
+              <select value={shelf} onChange={moveBook.bind(null, shelf)}>
+                <option value="moveTo" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{title}</div>
+          <div className="book-authors">{authors[0]}</div>
+        </div>)
 }
+
+Book.propTypes = {
+	book: PropTypes.object.isRequired,
+	onShelfChange: PropTypes.func.isRequired
+}
+
+export default Book
--- src/Books.js
+++ src/Books.js
@@ -1,60 +1,59 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import sortBy from 'sort-by';
 import Book from './Book';
 
-class Books extends Component {
-	static propTypes = {
-	    books: PropTypes.array.isRequired,
-	    onShelfChange: PropTypes.func.isRequired
+const Books = props => {
+	const onShelfChange = (book, shelf) => {
+		props.onShelfChange(book, shelf);
 	}
-	onShelfChange(book, shelf) {
-		this.props.onShelfChange(...arguments);
+	const books = props.books;
+	const shelfs = {
+		'currentlyReading': { title: 'Currently Reading', books: [] } ,
+		'read': { title: 'Read', books: [] },
+		'wantToRead': { title: 'Want to Read', books: [] }
 	}
-	render() {
-		const books = this.props.books;
-		const shelfs = {
-			'currentlyReading': { title: 'Currently Reading', books: [] } ,
-			'read': { title: 'Read', books: [] },
-			'wantToRead': { title: 'Want to Read', books: [] }
-		}
-		books.sort(sortBy('title'));
-		books.forEach(book => {
-			const { shelf } = book;
-			shelfs[shelf].books.push(book);
-		})
-		return (
-			<div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-              	{Object.keys(shelfs).map(shelf => 
-              		<div key={shelf} className="bookshelf">
-			          <h2 className="bookshelf-title">{shelfs[shelf].title}</h2>
-			          <div className="bookshelf-books">
-			            <ol className="books-grid">
-			            	{shelfs[shelf].books.map(book => 
-			            		<li key={book.id}>
-									<Book 
-										book={book}
-										onShelfChange={this.onShelfChange.bind(this, book)}
-									/>
-								</li>
-			            	)}
-			            </ol>
-			          </div>
-			        </div>
-              	)}
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to='/search'>Add a book</Link>
-            </div>
+	books.sort(sortBy('title'));
+	books.forEach(book => {
+		const { shelf } = book;
+		if (shelfs[shelf]) shelfs[shelf].books.push(book);
+	})
+	return (
+		<div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+          	{Object.keys(shelfs).map(shelf => 
+          		<div key={shelf} className="bookshelf">
+		          <h2 className="bookshelf-title">{shelfs[shelf].title}</h2>
+		          <div className="bookshelf-books">
+		            <ol className="books-grid">
+		            	{shelfs[shelf].books.map(book => 
+		            		<li key={book.id}>
+								<Book 
+									book={book}
+									onShelfChange={onShelfChange.bind(null, book)}
+								/>
+							</li>
+		            	)}
+		            </ol>
+		          </div>
+		        </div>
+          	)}
           </div>
-        )
-	}
+        </div>
+        <div className="open-search">
+          <Link to='/search'>Add a book</Link>
+        </div>
+      </div>
+    )
+}
+
+Books.propTypes = {
+	books: PropTypes.array.isRequired,
+	onShelfChange: PropTypes.func.isRequired
 }
 export default Books
